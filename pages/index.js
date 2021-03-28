40effa157b6f5c6c42369356417de947cdd6e5a4
germaneagle0 <thiagojuca@yahoo.com.br>
2021-03-28 03:11:00 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1> Home </h1>
+
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1)
+
+    function adicionarContador(){
+        setContador(contador + 1);
+    }
+    
+    return (
+        <div>
+            <button onClick={adicionarContador}>
+                CLICA EM MIM
+            </button>
+            <div>{contador}</div>
+        </div>
+    )
+}
+
+export default Home;
