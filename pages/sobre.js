ab41b2a6a71b25f56ffc8feffe6a6dd55dd87723
Thiago Oliveira Jucá Vasconcelos <thiagojuca@yahoo.com.br>
2021-03-28 01:05:59 -0300

    Attempt 3

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Link from 'next/link';
 
 function Sobre() {
@@ -13,22 +12,4 @@ function Sobre() {
         </div>
     )
 }
-
-function Contador() {
-    const [contador, setContador] = useState(1)
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-    
-    return (
-        <div>
-            <button onClick={adicionarContador}>
-                CLICA EM MIM
-            </button>
-            <div>{contador}</div>
-        </div>
-    )
-}
-
 export default Sobre;
