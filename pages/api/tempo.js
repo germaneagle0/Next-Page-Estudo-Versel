06ce4cdfb54761cb9ab0ce3f61e1dad887a87945
Thiago Oliveira Jucá Vasconcelos <thiagojuca@yahoo.com.br>
2021-03-29 23:05:03 -0300

    next advanced

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,17 @@
-function tempo(request, response) {
-    const dynamicDate = new Date()
+async function tempo(request, response) {
+
+    const dynamicDate = new Date();
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+    
+    const subscriberResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`)
+    const subscriberResponseJson = await subscriberResponse.json()
+    const inscritos = subscriberResponseJson.total_subscribers;
+
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
 
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        inscritos: 1
     })
 }
 
---
+++ pages/produtos/[id].js
@@ -0,0 +1,38 @@
+export async function getStaticPaths() {
+    return {
+        paths: [{
+            params: {
+                id: '1'                
+            }
+        },{
+            params: {
+                id: "2"
+            }
+        },{
+            params: {
+                id: "3"
+            }
+        }],
+        fallback: 'blocking',
+    }
+}
+
+export async function getStaticProps(context) {
+    const id = context.params.id;
+
+    return {
+        props: {
+            id: id
+        }
+    }
+}
+
+function Produtos(props) {
+    return (
+        <div>
+            {props.id}
+        </div>
+    )
+}
+
+export default Produtos;
